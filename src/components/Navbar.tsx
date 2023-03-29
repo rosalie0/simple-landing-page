b2786ac01baeedf0e93132c9fd510a0b4de32fdf
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 12:02:38 -0400

    navbar for mobile written. now have to add logic to decide which to use

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,33 @@
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center h-24 max-w-7xl mx-auto px-4">
-      <h1 className="text-3xl font-bold text-pink-300 w-full">REACT.</h1>
-      <ul className="text-white flex">
+    <div className="text-white flex justify-between items-center h-24 max-w-7xl mx-auto px-4">
+      {/* for large screens */}
+      <ul className="uppercase flex hidden">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
+      <div>
+        <AiOutlineMenu />
+      </div>
+
+      {/* for small screens */}
+      <div className="fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900">
+        <h1 className="p-4 text-3xl font-bold text-pink-300 w-full">REACT.</h1>
+
+        <ul className="pt-4 uppercase">
+          <li className="p-4 border-b border-gray-600 ">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600 ">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
