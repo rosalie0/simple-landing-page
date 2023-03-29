21218dbc0aba3e936964559b2e975818856782f0
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 13:17:07 -0400

    typed is so cool

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import Typed from "typed.js";
 
 const Hero = () => {
+  // Create reference to store the DOM element containing the typing animation
+  const el = React.useRef(null);
+  // useEffect for typing animation
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["BTB", "BTC", "SASS", "cat food"],
+      typeSpeed: 120,
+      backSpeed: 140,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="text-white">
       <div className="max-w-3xl mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
@@ -12,8 +30,9 @@ const Hero = () => {
         </h2>
         <div>
           <p className="md:text-5xl sm:text-4xl text-xl">
-            Fast, flexible financing for
+            Fast, flexible financing for <span ref={el} />
           </p>
+          <p></p>
         </div>
       </div>
     </div>
