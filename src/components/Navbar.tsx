3ae7752426abc30b517a8d152a8c9a4ede1019d0
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 12:21:41 -0400

    navbar opens and closes w button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,12 @@ const Navbar = () => {
     <div className="text-white flex justify-between items-center h-24 max-w-7xl mx-auto px-4">
       {/* for large screens */}
       <h1 className="p-4 text-3xl font-bold text-pink-300 w-full">REACT.</h1>
-      <ul className="uppercase flex hidden">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+      <ul className="uppercase flex">
+        <li className="p-4 cursor-pointer">Home</li>
+        <li className="p-4 cursor-pointer">Company</li>
+        <li className="p-4 cursor-pointer">Resources</li>
+        <li className="p-4 cursor-pointer">About</li>
+        <li className="p-4 cursor-pointer">Contact</li>
       </ul>
       <div onClick={handleNav}>
         {!navIsOpen ? (
@@ -27,14 +27,26 @@ const Navbar = () => {
       </div>
 
       {/* for small screens */}
-      <div className="fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900">
+      <div
+        className={
+          !navIsOpen
+            ? "fixed hidden"
+            : "fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900"
+        }
+      >
         <h1 className="p-4 text-3xl font-bold text-pink-300 w-full">REACT.</h1>
         <ul className="pt-4 uppercase">
-          <li className="p-4 border-b border-gray-600 ">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600 ">About</li>
-          <li className="p-4">Contact</li>
+          <li className="p-4 cursor-pointer border-b border-gray-600 ">Home</li>
+          <li className="p-4 cursor-pointer border-b border-gray-600">
+            Company
+          </li>
+          <li className="p-4 cursor-pointer border-b border-gray-600">
+            Resources
+          </li>
+          <li className="p-4 cursor-pointer border-b border-gray-600 ">
+            About
+          </li>
+          <li className="p-4 cursor-pointer">Contact</li>
         </ul>
       </div>
     </div>
