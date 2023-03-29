5d653c9509a71d354cdc35203f1a43aba896d5a0
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 14:09:36 -0400

    image and text is grid. the text is flex. styled. button

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import Laptop from "../assets/laptop.jpg";
 const Analytics = () => {
-  return <div>Analytics</div>;
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-6xl mx-auto grid md:grid-cols-2 ">
+        <img
+          src={Laptop}
+          alt="drawing of a laptop"
+          className="w-[500px] mx-auto my-4"
+        />
+        <div className="flex flex-col justify-center">
+          <p className="text-pink-300 uppercase font-bold">
+            Data analytics dashboard
+          </p>
+          <h2 className="font-bold md:text-4xl sm:text-3xl text-2xl py-2">
+            Manage Data Analytics Centrally
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
+            voluptatibus id facilis sapiente doloremque iste, amet incidunt
+            optio distinctio modi mollitia molestias magni itaque sit
+            consectetur reprehenderit laborum deserunt quae.
+          </p>
+          <button className="self-center bg-black font-medium my-6 rounded-md w-[200px] py-3 text-pink-300 ">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Analytics;
