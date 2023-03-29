1c756ba04d043315f2f49747f77a2f32d7473c74
Rosalie Newman <rosalienewman0@gmail.com>
2023-03-29 12:57:12 -0400

    navbar is supposedly done. has a bug in ease out transition that i suspect is solved later. so done, for now.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,14 +11,17 @@ const Navbar = () => {
     <div className="text-white flex justify-between items-center h-24 max-w-7xl mx-auto px-4">
       {/* for large screens */}
       <h1 className="p-4 text-3xl font-bold text-pink-300 w-full">REACT.</h1>
-      <ul className="uppercase flex">
+      <ul className="hidden  md:flex uppercase">
         <li className="p-4 cursor-pointer">Home</li>
         <li className="p-4 cursor-pointer">Company</li>
         <li className="p-4 cursor-pointer">Resources</li>
         <li className="p-4 cursor-pointer">About</li>
         <li className="p-4 cursor-pointer">Contact</li>
       </ul>
-      <div onClick={handleNav}>
+
+      {/* hide the hamburger icon on medium (and larger) screens.
+      Icon changed between hamburger and X depending on isNavOpen */}
+      <div onClick={handleNav} className="block md:hidden">
         {!navIsOpen ? (
           <AiOutlineMenu size={20} />
         ) : (
@@ -29,13 +32,17 @@ const Navbar = () => {
       {/* for small screens */}
       <div
         className={
-          !navIsOpen
-            ? "fixed hidden"
-            : "fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900"
+          navIsOpen
+            ? "fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r border-r-gray-900 ease-in-out duration-500"
+            : // Cannot do hidden or else we cannot apply transitions.
+              //If you do opacity-0 , it fades in. doing left -100 makes it move in from the left.
+              "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="p-4 text-3xl font-bold text-pink-300 w-full">REACT.</h1>
         <ul className="pt-4 uppercase">
+          <h1 className="p-4 text-3xl font-bold text-pink-300 w-full">
+            REACT.
+          </h1>
           <li className="p-4 cursor-pointer border-b border-gray-600 ">Home</li>
           <li className="p-4 cursor-pointer border-b border-gray-600">
             Company
